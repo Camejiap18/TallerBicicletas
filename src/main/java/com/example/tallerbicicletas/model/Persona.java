cac269150d34e349f1ff3bac5b9a8bfa2b012acd
Camila Mejia. <mariac.mejiap@uqvirtual.edu.co>
2026-02-28 11:12:53 -0500

    commit clase Persona.java

---
+++ src/main/java/com/example/tallerbicicletas/model/Persona.java
@@ -0,0 +1,23 @@
+package com.example.tallerbicicletas.model;
+
+public class Persona {
+
+    private String nombre;
+    private String identificacion;
+
+    public Persona(String nombre, String identificacion){
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getIdentificacion(){
+        return identificacion;
+    }
+
+
+
+}
