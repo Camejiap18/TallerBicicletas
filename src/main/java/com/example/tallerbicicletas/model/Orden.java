1a8a4a6143db3a41e1bb4942f6bde1654adaba87
Camila Mejia. <mariac.mejiap@uqvirtual.edu.co>
2026-02-28 20:26:39 -0500

    commit modificación clase órden

--- src/main/java/com/example/tallerbicicletas/model/Orden.java
+++ src/main/java/com/example/tallerbicicletas/model/Orden.java
@@ -1,5 +1,9 @@
 package com.example.tallerbicicletas.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Orden {
 
     //Atributos de la órden
@@ -8,19 +12,58 @@ public class Orden {
     private Mecanico mecanico;
     private String descripcionProblema;
     private String estado;
+    private LocalDate fechaIngreso;
+    private LocalTime horaIngreso;
+    private String motivoServicio;
+    private String diagnostico;
+    private ArrayList<String> trabajosRealizados;
+    private double costoTotal;
 
     //Constructor
-    public Orden(Cliente cliente, Bicicleta bicicleta, Mecanico mecanico, String descripcionProblema) {
+    public Orden(Cliente cliente, Bicicleta bicicleta, Mecanico mecanico,
+                 String descripcionProblema, String motivoServicio) {
+
         this.cliente = cliente;
         this.bicicleta = bicicleta;
         this.mecanico = mecanico;
+
         this.descripcionProblema = descripcionProblema;
+        this.motivoServicio = motivoServicio;
+
+        this.fechaIngreso = LocalDate.now();
+        this.horaIngreso = LocalTime.now();
+
+        this.diagnostico = " ";
+        this.trabajosRealizados = new ArrayList<>();
+        this.costoTotal = 0.0;
+
         this.estado = "Pendiente";
+    }
+
+    //Método para agregar un trabajo
+    public void agregarTrabajo(String trabajo, double costo) {
+        if (trabajo != null && !trabajo.isEmpty() && costo > 0) {
+            trabajosRealizados.add(trabajo);
+            costoTotal += costo;
+            if (estado.equals("Pendiente")) {
+                estado = "En proceso";
+            }
+        }
     }
 
     //Método para actualizar el estado de la óden
     public void cambiarEstado(String nuevoEstado) {
         this.estado = nuevoEstado;
+    }
+
+    //Método para finalizar la órdden
+    public void cerrarOrden(String diagnosticoFinal) {
+        if (!trabajosRealizados.isEmpty()) {
+            this.diagnostico = diagnosticoFinal;
+            this.estado = "Finalizada";
+        } else {
+            System.out.println("No se puede cerrar una orden sin trabajos realizados.");
+        }
     }
 
     //Getters y settters
