43de46b16cde3d99fea89f1ec2483d82ab22ff22
Camila Mejia. <mariac.mejiap@uqvirtual.edu.co>
2026-02-28 19:35:01 -0500

    commit modificación tamaño ventana

--- src/main/java/com/example/tallerbicicletas/HelloApplication.java
+++ src/main/java/com/example/tallerbicicletas/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.tallerbicicletas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,13 +13,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/tallerbicicletas/hello-view.fxml")
-        );
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();   // ← AQUÍ está la clave
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Bienvenido");
         stage.setScene(scene);
+        stage.sizeToScene();   // ajusta la ventana al contenido
+        stage.setResizable(false); // opcional
         stage.show();
     }
 }
